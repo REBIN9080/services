12b0113ed99ceb941452bcbf8ac31aa2f0bc6fa4
REBIN9080 <rebinsj95@gmail.com>
2022-07-27 11:19:52 +0530

    Chess-gold-thump-project

---
+++ Chess/src/com/goldthump/chess/ChessConstants.java
@@ -0,0 +1,16 @@
+package com.goldthump.chess;
+
+public interface ChessConstants {
+	final static String  bRook = "rook-black"; 
+	final static String  wRook = "rook-white"; 
+	final static String  bKnight ="knight-black"; 
+	final static String  wKnight ="knight-white"; 
+	final static String  bBishop = "bishop-black";
+	final static String  wBishop =  "bishop-white";
+	final static String  bQueen = "queen-black";
+	final static String  wQueen = "queen-white"; 
+	final static String  bKing =  "king-black";
+	final static String  wKing =   "king-white";
+	final static String  bPawn =  "pawn-black";
+	final static String  wPawn =  "pawn-white";
+}
---
+++ Chess/tst/com/goldthump/chess/ChessModelTest.java
@@ -0,0 +1,48 @@
+package com.goldthump.chess;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ChessModelTest {
+	
+	@Test
+	void testReset() {
+		
+		ChessModel chessModel = new ChessModel();
+		chessModel.reset();
+		chessModel.movePiece(0, 1, 0, 2);
+		chessModel.reset();
+		assertNull(chessModel.pieceAt(0, 2));
+		System.out.println(chessModel);
+	}
+	@Test
+	void testMovePiece() {
+		ChessModel chessModel = new ChessModel();
+		chessModel.reset();
+		assertNull(chessModel.pieceAt(0, 2));
+		
+		chessModel.movePiece(0, 1, 0, 2);
+		assertNotNull(chessModel.pieceAt(0, 2));
+	}
+	
+	@Test
+	void testPieceAt() {
+		ChessModel chessModel = new ChessModel();
+		assertNull(chessModel.pieceAt(0, 0));
+		chessModel.reset();
+		assertNotNull(chessModel.pieceAt(0, 0));
+		assertEquals(Player.WHITE, chessModel.pieceAt(0, 0).getPlayer());
+		assertEquals(Rank.ROOK, chessModel.pieceAt(0, 0).getRank());
+	}
+	@Test
+	void testToString() {
+		ChessModel chessModel = new ChessModel();
+		assertTrue(chessModel.toString().contains("0 .  .  .  .  .  .  .  . "));
+		chessModel.reset();
+		assertTrue(chessModel.toString().contains("0  r  n  b  q  k  b  n  r"));
+		
+	}
+
+}
